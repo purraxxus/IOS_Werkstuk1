bda204b25d3a48853ffc926282c3152e6b05b962
student <thatcronosdude@outlook.com>
2018-04-23 15:22:23 +0200

    error fix

--- Werkstuk1/HomeViewController.swift
+++ Werkstuk1/HomeViewController.swift
@@ -15,10 +15,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     let persoon1 = Personen(naam: "Amerijckx", voorNaam: "Kobe", foto: "kobe.jpg", adres: "Vollezelstraat 97 1755 Oetingen", gpsCoordinatenLat: 50.7688826, gpsCoordinatenLong: 4.049691400000029, telefoonNummer: 0486210707)
     let persoon2 = Personen(naam: "Tordeur", voorNaam: "Kelly", foto: "kelly.jpg", adres: "Gaasbeekstraat 1 1600 Oudenaken", gpsCoordinatenLat: 50.780032959999999, gpsCoordinatenLong: 4.197631099999967,telefoonNummer: 0478709610)
     let persoon3 = Personen(naam: "Devits", voorNaam: "Brian", foto: "brian.jpg", adres: "Molenstraat 23 1570 Vollezele", gpsCoordinatenLat: 50.7661342, gpsCoordinatenLong: 4.0265190000000075,telefoonNummer: 04995512362)
-    let persoon4 = Personen(naam: "Amerijckx", voorNaam: "Kobe", foto: "kobe.jpg", adres: "Vollezelstraat 97 1755 Oetingen", gpsCoordinatenLat: 50.7688826, gpsCoordinatenLong: 4.049691400000029, telefoonNummer: 0486210707)
-    let persoon5 = Personen(naam: "Tordeur", voorNaam: "Kelly", foto: "kelly.jpg", adres: "Gaasbeekstraat 1 1600 Oudenaken", gpsCoordinatenLat: 50.780032959999999, gpsCoordinatenLong: 4.197631099999967,telefoonNummer: 0478709610)
-    let persoon6 = Personen(naam: "Devits", voorNaam: "Brian", foto: "brian.jpg", adres: "Molenstraat 23 1570 Vollezele", gpsCoordinatenLat: 50.7661342, gpsCoordinatenLong: 4.0265190000000075,telefoonNummer: 04995512362)
-
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
@@ -44,9 +40,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         personenLijst.append(persoon1)
         personenLijst.append(persoon2)
         personenLijst.append(persoon3)
-        personenLijst.append(persoon4)
-        personenLijst.append(persoon5)
-        personenLijst.append(persoon6)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
