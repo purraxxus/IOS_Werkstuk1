099c5c4af229658cacf506907569960174f2b749
student <thatcronosdude@outlook.com>
2018-04-24 16:59:02 +0200

    mapkit work

--- Werkstuk1/MapViewController.swift
+++ Werkstuk1/MapViewController.swift
@@ -8,17 +8,26 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate{
 
     @IBOutlet var map: MKMapView!
     
     let distanceFromMapMarker:CLLocationDegrees = 100000
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let coordinates = CLLocationCoordinate2DMake(50.822557224337245,4.362658431445311)
-        map.setRegion(MKCoordinateRegionMakeWithDistance(coordinates, distanceFromMapMarker, distanceFromMapMarker), animated: true)
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        let coordinates = locationManager.location?.coordinate
+        map.setRegion(MKCoordinateRegionMakeWithDistance(coordinates!, distanceFromMapMarker, distanceFromMapMarker), animated: true)
         for value in personenLijst {
             let coordinatesPersoon = CLLocationCoordinate2DMake(value.gpsCoordinatenLat,value.gpsCoordinatenLong)
             let pin = Annotation(title: value.adres, subtitle: value.voorNaam + " " + value.naam, coordinate: coordinatesPersoon)
